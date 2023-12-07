d02d5bc303c83ec526374f3b04f6bb6cc4af427f
MustafaItgenics <mustafa.itgenics@gmail.com>
2023-12-07 18:09:55 +0500

    react router crash course

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  let [counter, setCounter]=useState(0)
+  const addValue=()=>{
+   let moreValue= counter<5 ? counter+1 : 5;
+    setCounter( moreValue);
+  }
+  const decValue=()=>{
+    const newValue = counter > 0 ? counter - 1 : 0;
+    setCounter(newValue);
+  }
+  return (
+    <>
+    <h1>Chai aur React</h1>
+    <h2>Counter value :{counter}</h2>
+    <button onClick={addValue}>Add Value</button>
+    <br />
+    <button onClick={decValue}>decre value</button>
+    </>
+  )
+}
+
+export default App
